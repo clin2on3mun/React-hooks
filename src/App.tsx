d4bc1d2dd22cd73a587b7f88396cf92d7e50750a
clin20n3mun <munanaclinton@gmail.com>
2025-05-26 18:57:10 +0200

    Create React app and add boilerplate files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import MyApp from "./components/contexts/ContextLesson1";
+import MyApp2 from "./components/contexts/ContextLesson2";
+import MyApp3 from "./components/contexts/ContextLesson3";
+
+function App() {
+  return (
+    <>
+      <MyApp />
+      <MyApp2 />
+      <MyApp3 />
+    </>
+  );
+}
+
+export default App;
